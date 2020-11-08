4f9cd2a1836392129e09eb2284d5cdd953b42bac
NoaUnity <noassock@gmail.com>
2020-11-08 13:24:23 +0100

    add first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+    //#region requires
+
+    //#endregion requires
+
+    //#region DOM
+
+    navbarHeader = $("#navbarHeader");
+    headerNavbarToggler = $("#headerNavbarToggler");
+
+    
+    //#endregion DOM
+    
+    
+    //#region variables
+    
+    //#endregion variables
+    
+    //#region functions
+    
+    //#endregion functions
+    
+    //#region MAIN
+    
+    
+    
+    headerNavbarToggler.click(function(){
+        if(navbarHeader.hasClass("show")){
+            navbarHeader.toggleClass("show");
+        }
+        else{
+            navbarHeader.toggleClass("collapse");
+            navbarHeader.toggleClass("collapsing");
+            navbarHeader.animate({height: "245px"},400,function(){
+                navbarHeader.toggleClass("collapsing");
+                navbarHeader.toggleClass("collapse");
+                navbarHeader.toggleClass("show");
+                navbarHeader.css("height", "");
+            });
+
+        }
+        
+            
+    })
+
+    
+    
+    //#endregion MAIN
+
+})
